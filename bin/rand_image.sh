1785545397beaa43441584df28bad760410eeb38
yuanzhen <zenuo@protonmail.com>
2020-07-11 14:05:06 +0800

    commit

--- bin/rand_image.sh
+++ bin/rand_image.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# 目标路径
+image_path='/assets/img/'
+
 # 移动指定的图片到文件夹，并重命名为随机字符串
 
 # 检查参数
@@ -19,12 +22,12 @@ rand=`openssl rand -hex 12`
 
 # 将参数指定的文件移动到图片文件夹，并重命名
 mv "$1" ../assets/img/$rand.$extension && \
-echo ![$rand.$extension]\(/image/$rand.$extension\) && \
+echo ![$rand.$extension]\($image_path$rand.$extension\) && \
 
 # 若xclip存在（Linux系统）
 if [ -x "$(command -v xclip)" ]; then
-  echo ![$rand.$extension]\(/image/$rand.$extension\) | xclip -sel clip
+  echo ![$rand.$extension]\($image_path$rand.$extension\) | xclip -sel clip
 # 若pbcopy存在（macOS系统）
 elif [ -x "$(command -v pbcopy)" ]; then
-  echo ![$rand.$extension]\(/image/$rand.$extension\) | pbcopy
+  echo ![$rand.$extension]\($image_path$rand.$extension\) | pbcopy
 fi
