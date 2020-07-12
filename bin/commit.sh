9b576e4ec60d424f15844a107539591eba604e11
yuanzhen <zenuo@protonmail.com>
2020-07-12 21:53:41 +0800

    974c42663a2bad6e

--- bin/commit.sh
+++ bin/commit.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 git add . && \
-git commit -m "commit" && \
+git commit -m $(openssl rand -hex 8) && \
 git push && \
 bundle exec jekyll build && \
 cd _site && \
 git add . && \
-git commit -m "commit" && \
+git commit -m $(openssl rand -hex 8) && \
 git push && \
 cd .. && \
 echo "$(date +%Y-%m-%d-%H-%M-%S) committed"
