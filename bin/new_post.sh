54cdb7e45e8acc28202959b635a51eb67fe2e117
yuanzhen <zenuo@protonmail.com>
2020-07-19 10:17:17 +0800

    添加输出、添加参数校验

--- bin/new_post.sh
+++ bin/new_post.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+if [[ "$#" -ne 1 ]]; then
+    echo "usage: "$0" <new post name>"
+    exit 1
+fi
+
 cd `dirname $0`
 date=`date +%Y-%m-%d`
+newFilePath="../_posts/$date-$@.md"
 echo '---
 layout: post
----' > "../_posts/$date-$@"
-
+---' > $newFilePath
+echo "new post created: '$(pwd)$newFilePath'"
+exit 0
